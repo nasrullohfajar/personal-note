5a53a799c6380b3b29b0e70dedb14efe69c83019
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-17 15:02:12 +0700

    add: button hover color

--- src/components/note/NoteItem.jsx
+++ src/components/note/NoteItem.jsx
@@ -4,7 +4,7 @@ import DeleteButton from "../button/DeleteButton";
 
 function NoteItem({ title, body, createdAt, id, onDelete }) {
   return (
-    <div className="note-item">
+    <div className="note-item shadow-md">
       <NoteItemBody title={title} body={body} createdAt={createdAt} />
       <DeleteButton id={id} onDelete={onDelete} />
     </div>
