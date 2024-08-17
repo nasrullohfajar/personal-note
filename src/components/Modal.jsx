3de86c5683e86260f9f949b0a9618fdf1f64f898
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-17 14:11:42 +0700

    fix modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,13 +5,12 @@ import { IoIosClose } from "react-icons/io";
 function Modal({ isOpen, addNote, isOpenHandler }) {
   return (
     isOpen && (
-      <div className="note-app__body note-input">
-        <h2>Buat Catatan</h2>
-        <NoteInput addNote={addNote} />
-
-        <button onClick={isOpenHandler}>
-          <IoIosClose />
+      <div className=" p-12 absolute inset-x-0 mt-20 items-center justify-center bg-black rounded-xl m-auto max-w-[500px]">
+        <button className="w-full " onClick={isOpenHandler}>
+          <IoIosClose className="ml-auto" size={24} />
         </button>
+        <h2 className="my-4">Buat Catatan</h2>
+        <NoteInput addNote={addNote} />
       </div>
     )
   );
